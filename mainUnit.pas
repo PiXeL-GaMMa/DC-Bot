49b2d5be6f825856637094c1ff5d68adfddd2496
Colin J.D. Stewart <81357919+PiXeL-GaMMa@users.noreply.github.com>
2021-10-21 23:11:34 +0300

    removed automatic useless includes

--- mainUnit.pas
+++ mainUnit.pas
@@ -16,8 +16,7 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, IdBaseComponent, IdComponent,
-  IdTCPConnection, IdTCPClient, Vcl.StdCtrls, ScktComp,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, ScktComp,
 
   System.ZLib;
 
